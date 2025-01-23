45bd7ae1ebe5a3a4db999a7cc3cb6efacc147ccc
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2025-01-23 20:55:11 -0300

    feat: Configuration, and brand and product classes

---
+++ src/main/kotlin/com/example/mongoApi/models/Brand.kt
@@ -0,0 +1,6 @@
+package com.example.mongoApi.models
+
+class Brand(
+    var name: String,
+    var headquarter: String
+)
---
+++ src/main/kotlin/com/example/mongoApi/models/Product.kt
@@ -0,0 +1,14 @@
+package com.example.mongoApi.models
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+
+@Document
+class Product(
+    @Id val id: String,
+    var name: String,
+    var manufacturer: Brand,
+    var productionDate: LocalDateTime,
+    var available: Boolean
+)
