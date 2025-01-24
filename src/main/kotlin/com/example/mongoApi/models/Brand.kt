7464c29669d6e822e26e1eab9ecc837fef304eac
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2025-01-23 23:42:37 -0300

    feat: Added a id field to brand and transformed it into a document

--- src/main/kotlin/com/example/mongoApi/models/Brand.kt
+++ src/main/kotlin/com/example/mongoApi/models/Brand.kt
@@ -1,6 +1,11 @@
 package com.example.mongoApi.models
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
 class Brand(
+    @Id val id: String,
     var name: String,
     var headquarter: String
 )
